ac9ff81bf9d8feba2a2a60b81088c95dc2f4e254
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-10-26 12:25:24 +0100

    Créer l'entité JPA Compte

---
+++ src/main/java/b/m29/ebankservice/entities/BankAccount.java
@@ -0,0 +1,27 @@
+package b.m29.ebankservice.entities;
+
+import b.m29.ebankservice.enums.AccountType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAT;
+    private Double balance;
+    private String currency;
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+
+
+}
---
+++ src/main/java/b/m29/ebankservice/enums/AccountType.java
@@ -0,0 +1,5 @@
+package b.m29.ebankservice.enums;
+
+public enum AccountType {
+    CURRENT_ACCOUNT,SAVINGS_ACCOUNT
+}
