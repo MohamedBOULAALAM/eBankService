61c2f05e50e69d5ac2dd821dd5f8e5fda6910d9d
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-10-26 12:12:16 +0100

    initial commit

---
+++ src/main/java/b/m29/ebankservice/EBankServiceApplication.java
@@ -0,0 +1,13 @@
+package b.m29.ebankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b/m29/ebankservice/EBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package b.m29.ebankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
