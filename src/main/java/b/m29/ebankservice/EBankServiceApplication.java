fcf35cb007ad0001e231bada919e7b36917ff1ed
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-10-27 11:55:56 +0100

    Créer un Web service GraphQL pour ce Micro-service : Ajout de la gestion des clients

--- src/main/java/b/m29/ebankservice/EBankServiceApplication.java
+++ src/main/java/b/m29/ebankservice/EBankServiceApplication.java
@@ -1,8 +1,10 @@
 package b.m29.ebankservice;
 
 import b.m29.ebankservice.entities.BankAccount;
+import b.m29.ebankservice.entities.Customer;
 import b.m29.ebankservice.enums.AccountType;
 import b.m29.ebankservice.repositories.BankAccountRepository;
+import b.m29.ebankservice.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static graphql.introspection.IntrospectionQueryBuilder.build;
 
@@ -21,6 +24,7 @@ public class EBankServiceApplication {
 	}
 
 	@Bean
+	/*
 	CommandLineRunner start(BankAccountRepository bankAccountRepository){
 		return args -> {
 			BankAccount bankAccount = null;
@@ -34,6 +38,31 @@ public class EBankServiceApplication {
 						.build();
 				bankAccountRepository.save(bankAccount);
 			}
+		};
+	}
+	*/
+	CommandLineRunner start(BankAccountRepository bankAccountRepository, CustomerRepository customerRepository) {
+		return args -> {
+			Stream.of("Mohamed","Yassin","Hanae").forEach(c -> {
+				Customer customer =  Customer.builder()
+						.name(c)
+						.build();
+				customerRepository.save(customer);
+			});
+			customerRepository.findAll().forEach(customer -> {
+				BankAccount bankAccount = null;
+				for (int i = 0; i < 10; i++) {
+					bankAccount = BankAccount.builder()
+							.id(UUID.randomUUID().toString())
+							.type(Math.random() > 0.29 ? AccountType.CURRENT_ACCOUNT : AccountType.SAVINGS_ACCOUNT)
+							.balance(588 + Math.random() * 29555)
+							.createdAT(new Date())
+							.currency("MAD")
+							.customer(customer)
+							.build();
+					bankAccountRepository.save(bankAccount);
+				}
+			});
 		};
 	}
 }
--- src/main/java/b/m29/ebankservice/entities/BankAccount.java
+++ src/main/java/b/m29/ebankservice/entities/BankAccount.java
@@ -6,14 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.Date;
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Entity @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class BankAccount {
     @Id
     private String id;
@@ -22,6 +17,6 @@ public class BankAccount {
     private String currency;
     @Enumerated(EnumType.STRING)
     private AccountType type;
-
-
+    @ManyToOne
+    private Customer customer;
 }
