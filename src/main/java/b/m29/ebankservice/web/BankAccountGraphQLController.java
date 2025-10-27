c32479c152e13a2c94660cdd3de71dbea816c74b
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-10-27 10:39:34 +0100

    Créer un Web service GraphQL pour ce Micro-service : Implémentation du Contrôleur GraphQL

---
+++ src/main/java/b/m29/ebankservice/web/BankAccountGraphQLController.java
@@ -0,0 +1,47 @@
+package b.m29.ebankservice.web;
+
+import b.m29.ebankservice.dto.BankAccountRequestDTO;
+import b.m29.ebankservice.dto.BankAccountResponseDTO;
+import b.m29.ebankservice.entities.BankAccount;
+import b.m29.ebankservice.repositories.BankAccountRepository;
+import b.m29.ebankservice.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private AccountService accountService;
+
+
+    @QueryMapping
+    public List<BankAccount> accountList(){ return bankAccountRepository.findAll();}
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){
+        return bankAccountRepository.findById( id)
+                .orElseThrow(()-> new RuntimeException(String.format("Account %s not found", id)));
+    }
+
+
+
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+        return accountService.addAccount(bankAccount);
+    }
+    @MutationMapping
+    public BankAccountResponseDTO updateAccount(@Argument String id,@Argument BankAccountRequestDTO bankAccount){
+        return accountService.updateAccount(id, bankAccount);
+    }
+    @MutationMapping
+    public Boolean deleteAccount(@Argument String id){
+        bankAccountRepository.deleteById(id);
+        return true;
+    }
+}
